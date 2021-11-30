73c16d2c44b8ba3c1d8ce2b58c32a4455d9e153a
jungkook <>
2021-11-30 15:20:35 +0300

    permissions were created

--- artwork/views.py
+++ artwork/views.py
@@ -4,6 +4,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from artwork.models import Entry
+from artwork.permissions import IsAuthorOrStaffOrReadOnly
 from artwork.serializers import EntrySerializer
 
 
@@ -15,8 +16,14 @@ class EntryViewSet(ModelViewSet):
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    permission_classes = [IsAuthorOrStaffOrReadOnly]
     filter_fields = ['title']
     search_fields = ['title', 'description']
     ordering_fields = ['author', 'title']
+
+    def perform_create(self, serializer):
+        serializer.validated_data['author'] = self.request.user
+        serializer.save()
+
 
 
