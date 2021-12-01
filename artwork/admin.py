dc4f9c83888e396cacc9bf3bed011c8ddf3b7b30
jungkook <>
2021-12-01 07:13:50 +0300

    ..

--- artwork/admin.py
+++ artwork/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from artwork.models import Entry
+from artwork.models import Entry, Comment
 
-admin.site.register(Entry)
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "published", "picture")
+    list_display_links = ("title",)
+
+
+@admin.register(Comment)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ("id", "owner", "entry", "text")
+    list_display_links = ("entry",)
--- artwork/models.py
+++ artwork/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Entry(models.Model):
     title = models.CharField("Название", max_length=200)
-    picture = models.ImageField("арт-работа", upload_to='pictures/')
+    picture = models.ImageField("Арт-работа", upload_to='pictures/')
     author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
@@ -15,3 +15,17 @@ class Entry(models.Model):
     class Meta:
         verbose_name = "арт-работа"
         verbose_name_plural = "арт-работы"
+
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User, verbose_name="Автор", on_delete=models.CASCADE)
+    text = models.TextField("Комментарий")
+    entry = models.ForeignKey(Entry, verbose_name="Арт-работа", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.owner} - {self.entry}"
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        
